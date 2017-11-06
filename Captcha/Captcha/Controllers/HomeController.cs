d30bd8b409d98537532267e3818c26f780ae3d01
YOUR_USERNAME <cuongktbk@gmail.com>
2017-11-06 17:01:55 +0700

    6/11/2107
    6/11/2107

---
+++ Captcha/Captcha/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaptchaMvc.HtmlHelpers;
+
+namespace Captcha.Controllers
+{
+	public class HomeController : Controller
+	{
+		//
+		// GET: /Home/
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(string empty)
+		{
+			// Code for validating the CAPTCHA 
+			if (this.IsCaptchaValid("Captcha is not valid"))
+			{
+
+				return RedirectToAction("ThankYouPage");
+			}
+
+			ViewBag.ErrMessage = "Error: captcha is not valid.";
+			return View();
+
+		}
+
+		public ActionResult ThankYouPage()
+		{
+			return View();
+		}
+
+	}
+}
